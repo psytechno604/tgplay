efa47953beae0fb9ab922132015ac6cdefa34af6
Sergey Razumov <sergey.razumov@nowtilus.tv>
2021-07-03 08:14:11 +0200

    getDb function

--- services/database.service.js
+++ services/database.service.js
@@ -4,30 +4,36 @@ const { open } = require('sqlite')
 
 module.exports = {
   name: 'database',
-  metadata: {
+  methods: {
+    async getDb() {
+      if (this.metadata.db) {
+        return this.metadata.db
+      }
+      this.metadata.db = await open({
+        filename: path.join(process.env.DB_PATH, 'db.sqlite'),
+        driver: sqlite3.Database
+      })
+      await this.metadata.db.exec('CREATE TABLE IF NOT EXISTS T_DOWNLOADS (dlId TEXT PRIMARY KEY, createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP, serviceName TEXT, releaseUrl TEXT)')
+      process.on('SIGINT', () => {
+        this.metadata.db.close()
+      })
+      return this.metadata.db
+    }
   },
   actions: {
     async insertDlId(ctx) {
-      return this.metadata.db.run(`INSERT INTO T_DOWNLOADS (dlId, serviceName, releaseUrl) VALUES ($dlId, $serviceName, $releaseUrl)`, {
+      const db = await this.getDb()
+      return db.run(`INSERT INTO T_DOWNLOADS (dlId, serviceName, releaseUrl) VALUES ($dlId, $serviceName, $releaseUrl)`, {
         $dlId: ctx.params.dlId,
         $serviceName: ctx.params.serviceName,
         $releaseUrl: ctx.params.releaseUrl
       })
     },
     async dlIdExists(ctx) {
-      return this.metadata.db.get(`SELECT dlId FROM T_DOWNLOADS WHERE dlId=$dlId`, {
+      const db = await this.getDb()
+      return db.get(`SELECT dlId FROM T_DOWNLOADS WHERE dlId=$dlId`, {
         $dlId: ctx.params.dlId
       })
     }
-  },
-  async started() {
-    this.metadata.db = await open({
-      filename: path.join(process.env.DB_PATH, 'db.sqlite'),
-      driver: sqlite3.Database
-    })
-    await this.metadata.db.exec('CREATE TABLE IF NOT EXISTS T_DOWNLOADS (dlId TEXT PRIMARY KEY, createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP, serviceName TEXT, releaseUrl TEXT)')
-    process.on('SIGINT', () => {
-      this.metadata.db.close()
-    })
   }
 }
